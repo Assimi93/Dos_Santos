c2b90b61b068be2e5a9fe0cfbf8aa673eaf1d340
Assimi93 <127421748+Assimi93@users.noreply.github.com>
2024-03-13 10:08:47 +0100

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,20 @@
+/*-----------------------------------
+            DEMBELE
+            Assimi
+
+            Seance 2
+            exercice 1
+-----------------------------------*/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+        
+
+int main() {
+    cout << "Hello Nigg*!" << endl;
+    return 0;
+    }
+    
